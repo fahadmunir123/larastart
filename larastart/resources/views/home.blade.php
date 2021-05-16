06f129dec134932719b2af54291d7d9a5fe8a092
fahadmunir123 <akontolive@gmail.com>
2021-05-16 22:03:26 +0500

    commit

--- larastart/resources/views/home.blade.php
+++ larastart/resources/views/home.blade.php
@@ -14,7 +14,7 @@
                         </div>
                     @endif
 
-                    {{ __('You are logged in!') }}
+                    {{ __('You are logged in! Mr fahad munir') }}
                 </div>
             </div>
         </div>
